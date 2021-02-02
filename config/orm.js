eb9c65c1c0c72481bf6de40b99bcea599295a5b6
jmerges bootcamp <jamesmerges1@gmail.com>
2021-02-02 15:14:05 -0800

    selectAll orm method

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,12 @@
+const connection = require("./connection.js");
 var connction = require("./connection.js");
 
 var orm = {
-    
+    selectAll: function(table, cb) {
+        queryString = "SELECT * FROM ??";
+        connection.query(queryString, [table], function(err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    }
 }
