ca4b85088f6a0d30a6fc03d5c5298d681f228ce4
jmerges bootcamp <jamesmerges1@gmail.com>
2021-02-02 15:58:40 -0800

    set up server.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var burger = require('../models/burger.js');
+
+var router = express.Router();
+
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+
+    });
+});
+
+module.exports = router;
