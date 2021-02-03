e09612830cadc7b0daf9607fdb129c71f9cd9241
jmerges bootcamp <jamesmerges1@gmail.com>
2021-02-02 23:19:48 -0800

    Heroku settings

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,18 @@
 var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password:"password",
-    port: 3306,
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(procss.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password:"password",
+        port: 3306,
+        database: "burgers_db"
+    });
+}
+
 
 module.exports = connection;
